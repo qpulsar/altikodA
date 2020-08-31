bff179ff6d750dfc63f3b2f9a739eb06a51972d8
Mehmet Emin Korkusuz <ekorkusuz@gmail.com>
2020-08-31 21:16:59 +0300

    16

--- 016_topla.py
+++ 016_topla.py
@@ -1,1 +1,28 @@
 # İki sayıyı topla
+from tkinter import *
+
+
+def topla():
+    try:
+        s1 = int(sayi1.get())
+        s2 = int(sayi2.get())
+        sonuc["text"] = s1+s2
+    except:
+        print("sayı dönüşümünde hata.")
+
+root = Tk()
+root.title("İki sayıyı topla")
+root.geometry("300x300-1200+50")
+
+sayi1 = Entry(root)
+sayi1.pack()
+sayi2 = Entry(root)
+sayi2.pack()
+
+but = Button(text="+", command=topla)
+but.pack()
+
+sonuc = Label(text="")
+sonuc.pack()
+
+root.mainloop()
