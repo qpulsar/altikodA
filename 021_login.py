c3f2d2265e28a6630ccfbec4b47d55fb684929b2
Mehmet Emin Korkusuz <ekorkusuz@gmail.com>
2020-09-19 22:33:25 +0300

    21

--- 021_login.py
+++ 021_login.py
@@ -1,5 +1,32 @@
 from tkinter import *
 
+uname = "e"
+upass = "a"
+
+
+def ekle(event):
+    global madde_degiskeni, liste
+    liste.insert(0, madde_degiskeni.get())
+    madde_degiskeni.set("")
+
+
+def kontrol():
+    global madde_degiskeni, liste
+    madde_degiskeni = StringVar()
+    if uname == isim_degiskeni.get() and upass == pass_degiskeni.get():
+        yeni_pencere = Toplevel(root)
+        yeni_pencere.title("Asıl program")
+        yeni_pencere.geometry("300x300+1300+100")
+        root.withdraw()
+        madde = Entry(yeni_pencere, textvariable=madde_degiskeni)
+        madde.bind('<Return>', ekle)
+        madde.pack()
+        liste = Listbox(yeni_pencere)
+        liste.pack()
+    else:
+        print("Sen kimsin!")
+
+
 root = Tk()
 root.title("Login Page")
 root.geometry("300x250+1200+50")
@@ -10,11 +37,10 @@ Label(root, text="İsminizi giriniz").pack()
 isim = Entry(root, textvariable=isim_degiskeni)
 isim.pack()
 Label(root, text="Parolanızı giriniz").pack()
-parola = Entry(root, textvariable=pass_degiskeni)
+parola = Entry(root, textvariable=pass_degiskeni, show="*")
 parola.pack()
 
-buton = Button(root, text="Login")
+buton = Button(root, text="Login", command=kontrol)
 buton.pack()
-
 
 root.mainloop()
